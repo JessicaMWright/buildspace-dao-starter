55c87bb5269712f3d1e18c1bc76fe64dde56b3b0
JessicaMWright <87302815+JessicaMWright@users.noreply.github.com>
2022-04-13 04:00:30 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -176,17 +176,8 @@ useEffect(() => {
       setIsClaiming(false);
     }
   };
-  if (network?.[0].data.chainid !== ChainId.Rinkeby) {
-    return (
-      <div className="unsupported-network">
-        <h2>Please connect to Rinkeby</h2>
-        <p>
-          This dapp only works on the Rinkeby network, please switch networks
-          in your connected wallet.
-        </p>
-      </div>
-    );
-  }
+  
+ 
   // This is the case where the user hasn't connected their wallet
   // to your web app. Let them call connectWallet.
   if (!address) {
